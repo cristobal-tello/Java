741f05c4ae87e2411e7e1775ed52768b2db5724d
Cristóbal Tello <cristobal.tello@t-systems.com>
2018-03-09 13:28:48 +0100

    Update

--- Java Spring Application Basics/03.Create a simple Spring Application/MySimpleSpringApp/src/main/java/mySimpleSpringApp/Fruit.java
+++ Java Spring Application Basics/03.Create a simple Spring Application/MySimpleSpringApp/src/main/java/mySimpleSpringApp/Fruit.java
@@ -1,6 +1,8 @@
 package mySimpleSpringApp;
 
 public class Fruit {
+	public int anyValue;
+	
 	public String talkAboutYourSelf()
 	{
 		return "Hi, I'm a FRUIT. I'm come from plants or trees with seeds";
--- Java Spring Application Basics/03.Create a simple Spring Application/MySimpleSpringApp/src/main/java/mySimpleSpringApp/myApp.java
+++ Java Spring Application Basics/03.Create a simple Spring Application/MySimpleSpringApp/src/main/java/mySimpleSpringApp/myApp.java
@@ -10,15 +10,36 @@ public class myApp {
 		// Fruit f = new Fruit();
 		// Vegetable v = new Vegetable();
 		
-		// Using spring
+		// Using spring if the file is on the root of project folder
 		ApplicationContext appContext = new FileSystemXmlApplicationContext("appContext.xml");
+		//ApplicationContext appContenxt = new ClassPathXmlApplicationContext(<path here>);
+		// Make sure next object have a cctor without arguments or simply, doens't have a cctor.
+		
+		// Scope:
+		// Singleton: By default 
+		// Prototype: Multiple instances
+		// Request: One instance per HTTP request
+		// Session: One instace per HTTP session
+		// Global session: One instance per Global HTTP session
+		
 		// Using class
 		Fruit f = appContext.getBean("unique_id_fruit", Fruit.class);
+		f.anyValue = 3;
+		System.out.println("f.anyValue="+f.anyValue);
+		
 		// Or using cast
 		Vegetable v = (Vegetable) appContext.getBean("unique_id_vegetable");
 		
 		System.out.println(f.talkAboutYourSelf());
 		System.out.println(v.talkAboutYourSelf());
+		
+		System.out.println("\r\n");
+		System.out.println("Scopes");
+		
+		Fruit f2 = appContext.getBean("unique_id_fruit", Fruit.class);
+		System.out.println("f2.anyValue="+f.anyValue);
+		System.out.println(f2.talkAboutYourSelf());
+		
 	}
 
 }
